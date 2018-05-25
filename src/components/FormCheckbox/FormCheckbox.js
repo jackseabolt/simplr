a3f9ba3cb5703d108f2eeb2d713281c75b030b58
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-05-25 10:29:10 -0700

    role removed

--- src/components/FormCheckbox/FormCheckbox.js
+++ src/components/FormCheckbox/FormCheckbox.js
@@ -3,7 +3,7 @@ import './FormCheckbox.css';
 
 const FormCheckbox = props => {
     return (
-        <section role="region" className={props.error ? "error" : ""}>
+        <section className={props.error ? "error" : ""}>
             <h4>{props.title}</h4>
             <label className="container">I have read and do accept <a href={props.link}>{props.topic}</a>
                 <input type="checkbox" onChange={event => props.onChange(event)}/>
