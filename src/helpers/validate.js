d5776b95543a748f3948f84770a4fcc44b647572
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-05-25 15:52:36 -0700

    email validation

--- src/helpers/validate.js
+++ src/helpers/validate.js
@@ -1,11 +1,21 @@
 const validate = (email, business_name, username, type, terms, policy, password, passFunc) => {
     return new Promise((resolve, reject) => {
-        email ? passFunc({ error_business_email: null }) : passFunc({ error_business_email: "Please enter a valid business email" }); 
         business_name ? passFunc({ error_business_name: null }) : passFunc({ error_business_name: "Please enter a valid business address" });
         username ? passFunc({ error_username: null }) : passFunc({ error_username: "Please enter a valid username" });
         type ? passFunc({ error_type: null }) : passFunc({ error_type: true });
         terms ? passFunc({ error_terms: null }) : passFunc({ error_terms: "Please agree to the terms of service" });
         policy ? passFunc({ error_policy: null }) : passFunc({ error_policy: "Please agree to our privacy policy" });
+        if(email) {
+            if(/@/.test(email)) {
+                passFunc({ error_business_email: null }) 
+            }
+            else {
+                passFunc({ error_business_email: "Please enter a valid business email" })
+            }
+        }
+        else {
+            passFunc({ error_business_email: "Please enter a valid business email" })            
+        }
         if(password) {
             // checking password length
             if(password.length >= 6) {
